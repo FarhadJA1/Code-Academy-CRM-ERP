d26b15142b2faa78d01d81aa3411fec51cefae49
Farhad Azadaliyev <farhadja@code.edu.az>
2022-05-11 14:14:54 +0400

    configurations prepared

---
+++ code-academy-backend/Api/Domain/Configurations/PayTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Configurations
+{
+    public class PayTypeConfiguration : IEntityTypeConfiguration<PayType>
+    {
+        public void Configure(EntityTypeBuilder<PayType> builder)
+        {
+            builder.Property(m => m.Name).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.SoftDelete).HasDefaultValue(false);
+        }
+    }
+}
---
+++ code-academy-backend/Api/Domain/Configurations/StudentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Configurations
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.Property(m => m.Name).IsRequired().HasMaxLength(100);
+            builder.Property(m => m.Surname).IsRequired().HasMaxLength(100);
+            builder.Property(m => m.CreateDate).HasDefaultValue(DateTime.Now);
+            builder.Property(m => m.Email).IsRequired().HasMaxLength(100);
+            builder.Property(m => m.SoftDelete).HasDefaultValue(false);
+            builder.Property(m => m.Surname).IsRequired().HasMaxLength(100);
+        }
+    }
+}
